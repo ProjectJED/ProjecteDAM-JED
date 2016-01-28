5c5e05a401f4485c1d08973f5485060a8f95cda7
Jordi Cubota Amate <cubota.jordi.inf2@gmail.com>
2016-01-28 18:46:14 +0100

    First commit

---
+++ desktop/src/com/jed/projectejed/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jed.projectejed.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jed.projectejed.JED;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new JED(), config);
+	}
+}
