78e332d747704ec3108bf2c32e4b86ad557ce8be
Thurisaz8 <ericgamo@gmail.com>
2016-02-04 18:48:35 +0100

    OrtographicCamera added

--- core/src/com/jed/projectejed/World/Renderer.java
+++ core/src/com/jed/projectejed/World/Renderer.java
@@ -1,7 +1,9 @@
 package com.jed.projectejed.World;
 
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.jed.projectejed.Helpers.AssetLoader;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
@@ -12,10 +14,16 @@ import com.jed.projectejed.Screens.MenuScreen;
 public class Renderer {
     private SpriteBatch batch;
     private int gameHeight, gameWidth;
+    private OrthographicCamera cam;
+
+
     public Renderer(int gameHeight, int midPointY, int gameWidth) {
         batch = new SpriteBatch();
         this.gameHeight = gameHeight;
         this.gameWidth = gameWidth;
+        cam = new OrthographicCamera();
+        cam.setToOrtho(true, 136, gameHeight);
+        batch.setProjectionMatrix(cam.combined);
     }
 
     public void render() {
